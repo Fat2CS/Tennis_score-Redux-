b5d4a7d06510d1b17bd9070c3a474d1892ba1e42
Fat2CS <coulibalyfatoumata@hotmail.com>
2023-02-28 12:45:32 +0100

    first

---
+++ src/PlayPauseButton.js
@@ -0,0 +1,17 @@
+import { useDispatch } from "react-redux";
+import { playPause } from "./store";
+
+export function PlayPauseButton() {
+  const dispatch = useDispatch();
+
+  return (
+    <button
+      onClick={() => {
+        dispatch(playPause());
+      }}
+    >
+      {" "}
+      Pause / Reprendre{" "}
+    </button>
+  );
+}
